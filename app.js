627ca136811cdc06fd1d67dace9bd9c651bba40f
melocotoneroh <aamarber@gmail.com>
2023-12-22 12:47:35 +0100

    Fixes for netlify

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 
 const express = require('express');
-const serverless = require('serverless-http');
 
 const app = express();
 
@@ -11,13 +10,10 @@ app.use(express.json());
 
 app.use(cors());
 
-module.exports = serverless(app);
+module.exports = app;
 
 require('./controllers/gameController');
 
 app.get('/welcome', (request, result) => {
     result.status(200).send("Welcome not logged in user!");
 });
-
-
-
