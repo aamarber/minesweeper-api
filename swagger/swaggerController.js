32f33c2ddaf5c1a8c8636507430ab22e808f0d1d
melocotoneroh <aamarber@gmail.com>
2024-01-10 10:38:18 +0100

    Removed not necessary code for swagger

--- swagger/swaggerController.js
+++ swagger/swaggerController.js
@@ -1,8 +1,6 @@
 const app = require('../app');
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger-output.json')
-const bodyParser = require('body-parser')
 
 /* Middlewares */
-app.use(bodyParser.json())
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
