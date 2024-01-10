f99147b2da28ae60b97522caba18680647c31c07
melocotoneroh <aamarber@gmail.com>
2024-01-10 11:13:04 +0100

    Recover swagger ui express version Set up static CDN rules for netlify

--- swagger/swaggerController.js
+++ swagger/swaggerController.js
@@ -2,5 +2,16 @@ const app = require('../app');
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger-output.json')
 
+const options = {
+    customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.min.js',
+        ],
+        customCssUrl: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.css',
+        ]
+}
+
 /* Middlewares */
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, options))
